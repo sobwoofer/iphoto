623c3d4dea5a51fa8847682800c1d46411d13075
Ivan Volynets <sobwoofer8@gmail.com>
2020-09-26 17:06:52 +0300

    view fixes

--- resources/views/template/partials/posts.blade.php
+++ resources/views/template/partials/posts.blade.php
@@ -1,5 +1,5 @@
 @foreach($posts as $post)
-    <div class="col-md-4">
+    <div class="col-md-4 text-center text-md-left">
         <img src="{{$post->cover->thumbnail(370, 200)}}" alt="blog image">
         <div class="minimalio-latest-blog-button">
             <button>Перейти</button>
